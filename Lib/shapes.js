6f4721876b2f159447738b0402181f769475ddf0
Maridon Romney <mkromney@gmail.com>
2023-06-29 00:14:15 -0600

    final commments added and code refined.

--- Lib/shapes.js
+++ Lib/shapes.js
@@ -1,5 +1,6 @@
 // These are the classes that define the shapes for the logo, they include the constructor and render methods. Each class takes the colour the user has selected. The shapes are filled with a style that includes the user's color choice. //
 
+// Using Shape as a parent class makes it so I don't have to call every single class over to the index.js file. Instead, I can reference Shape and it will in turn reference the child classes within its domain. //
 class Shape {
   constructor() {
     this.color = '';
@@ -27,28 +28,8 @@ class SquareShape extends Shape {
   }
 }
 
-class Logo {
-  constructor() {
-    this.text = '';
-    this.shape = '';
-  }
-
-  render() {
-    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">${this.shape}${this.text}</svg>`;
-  }
-
-  setText(text) {
-    this.text = `<text class="letters" x="50" y="60" text-anchor="middle" fill="white">${text}</text>`;
-  }
-
-  setShape(shape) {
-    this.shape = `<g class="shape">${shape.render()}</g>`;
-  }
-}
-
 module.exports = {
   Circle: CircleShape,
   Triangle: TriangleShape,
   Square: SquareShape,
-  Logo,
 };
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// These constant variables store the necessary paths to require resources in other. This includes the inquirer library, and the variable storing information about the shapes modules. //
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./Lib/shapes');
 
-
+// In this class we store the constructor and render methods which has two properties pulling from shapes.js, text and shape. //
 class Logo {
   constructor() {
     this.text = '';
     this.shape = '';
   }
 
+  // This render method give us the string that will represent the logo, below we set the parameter of that logo with the setText and setShape based on the user's input. //
   render() {
     return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">${this.shape}${this.text}</svg>`;
   }
@@ -22,6 +25,7 @@ class Logo {
   }
 }
 
+// Modelled after the inquirer library, this variable stores the user prompts and allowed selections. There is also an input prompt for three letters that make up the Text of the logo. //
 const questions = [
   {
     type: 'list',
@@ -48,6 +52,7 @@ const questions = [
   },
 ];
 
+// This function displays the questions to the user and then shows the user their selection as a confirmation. answers is broken down in order to take the selected logo attributes based on the classes stored in ./Lib/shapes.js. //
 inquirer.prompt(questions).then((answers) => {
   const { shape, color, letters } = answers;
   let selectedShape;
@@ -62,6 +67,7 @@ inquirer.prompt(questions).then((answers) => {
     selectedShape = new Square();
   }
 
+  // These variables store the logo with its final attributes as a string, the string that ends up being the .svg file. //
   selectedShape.setColorFunction(color);
 
   logo.setText(letters);
@@ -69,7 +75,7 @@ inquirer.prompt(questions).then((answers) => {
 
   chosenLogo = logo.render();
 
-
+// The fs.writeFile function writes the file and names it 'logo.svg' usign the newly stored string. //
   fs.writeFile('logo.svg', chosenLogo, (err) => {
     if (err) {
       console.error('Error writing logo.svg:', err);
